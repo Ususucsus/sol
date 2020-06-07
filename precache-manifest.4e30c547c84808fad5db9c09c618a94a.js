74c28264503508eb597006bb3d4ac43efe0e5ed2
Ususucsus <usarti01@gmail.com>
2020-06-07 12:39:31 +0300

    Updates

---
+++ precache-manifest.4e30c547c84808fad5db9c09c618a94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74987df421a6b3b7cfd2e94c1bff4c6c",
+    "url": "/sol/index.html"
+  },
+  {
+    "revision": "bc498f5db46d91ab7f5a",
+    "url": "/sol/static/css/main.d781c750.chunk.css"
+  },
+  {
+    "revision": "62e0dcd3d536adc0f99b",
+    "url": "/sol/static/js/2.6cfb117c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sol/static/js/2.6cfb117c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc498f5db46d91ab7f5a",
+    "url": "/sol/static/js/main.0778b108.chunk.js"
+  },
+  {
+    "revision": "078c2de7762ef84f1043",
+    "url": "/sol/static/js/runtime-main.9269ae5d.js"
+  }
+]);
